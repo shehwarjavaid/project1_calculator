7b39eaf370a9db36d8595fdccaaca53854ed7252
Shehwar Javaid <shehwarjavaid@gmail.com>
2024-02-12 21:24:55 +0500

    First Commit

---
+++ cal.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answers:{
+    numberOne:number,
+    numberTwo:number,
+    operator:string
+    
+    
+}=await inquirer.prompt([
+{
+    type:"number",
+    name:"numberOne",
+    message:"Kindly enter your first number"
+ 
+},{
+    type:"number",
+    name:"numberTwo",
+    message:"Kindly enter your second number"
+
+
+},{
+    type:"list",
+    name:"operator",
+    choices:["addition(+)","subtraction(-)","multiplication(*)","division(/)"],
+    message:"Select your operator:"
+
+}
+])
+const {numberOne,numberTwo,operator}=answers
+if(numberOne && numberTwo && operator){
+    let result:number=0;
+    if(operator ==="addition(+)"){
+        result=numberOne+numberTwo
+    }else
+ if(operator ==="subtraction(-)"){
+    result=numberOne-numberTwo
+}else if(operator ==="multiplication(*)"){
+    result=numberOne*numberTwo
+}else if(operator ==="division(/)")
+    result=numberOne/numberTwo
+    console.log("Your result is :",result)}
+else{console.log("kindly enter valid input")}
+
